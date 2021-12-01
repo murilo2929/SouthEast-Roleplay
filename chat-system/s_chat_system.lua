6b2b9908a72c6490bd480ef8bf46a7409f57cd05
murilo2929 <69065854+murilo2929@users.noreply.github.com>
2021-12-01 09:22:29 -0300

    sistema localizar veículo

--- chat-system/s_chat_system.lua
+++ chat-system/s_chat_system.lua
@@ -3099,3 +3099,47 @@ function pdmir(source)
 	end
 end
 addCommandHandler("mir", pdmir, false, false)
+
+local gpsBlips = {}
+local gpsMarkers = {}
+function gpsFunc(thePlayer, cmd, vehID)
+	local playerID = getElementData(thePlayer, "dbid")
+	if not vehID then
+		outputChatBox("#d91e1eAviso: #ffffff /" .. cmd .. " [Veiculo ID]", thePlayer, 194, 0, 0, true)
+		return false
+	end
+	if not gpsBlips[playerID] then
+		local vehicle = exports.pool:getElement("vehicle", vehID)
+		if vehicle then
+			local vehicleOwner = getElementData(vehicle, "owner")
+			if vehicleOwner == playerID then
+				local vehposX, vehposY, vehposZ = getElementPosition(vehicle)
+				gpsBlips[playerID] = createBlip(vehposX, vehposY, vehposZ, 19, 2, 255, 0, 0, 255, 0, 99999.0, thePlayer)
+				gpsMarkers[playerID] = createMarker(vehposX, vehposY, vehposZ, "checkpoint", 3, 255, 0, 0, 255, thePlayer)
+				attachElements(gpsMarkers[playerID], vehicle)
+				attachElements(gpsBlips[playerID], vehicle)
+				if gpsBlips[playerID] then
+					outputChatBox("#d91e1eAviso: #ffffffVocê pode seguir seu veículo a partir da marca da bandeira vermelha no mapa.", thePlayer, 0, 255, 0, true)
+				end
+			else
+				outputChatBox("#d91e1eAviso: #ffffffO veículo não é seu.", thePlayer, 0, 255, 0, true)
+			end
+		end
+	else
+		outputChatBox("#d91e1eAviso: #ffffffVocê já está localizando um veiculo. Utilize /eloc antes de poder usá-lo novamente.", thePlayer, 0, 255, 0, true)
+	end
+end
+addCommandHandler("vloc", gpsFunc)
+
+function kgpsFunc(thePlayer, cmd)
+	local playerID = getElementData(thePlayer, "dbid")
+	if gpsBlips[playerID] then
+		destroyElement(gpsMarkers[playerID])
+		gpsMarkers[playerID] = false
+		destroyElement(gpsBlips[playerID])
+		gpsBlips[playerID] = false
+		outputChatBox("#d91e1eAviso: #ffffffVocê desligou o GPS com sucesso.", thePlayer, 0, 255, 0, true)
+	end
+end
+addCommandHandler("eloc", kgpsFunc)
+
