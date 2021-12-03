7b32f1a65aee725b186eac64dde0f23630041336
murilo2929 <69065854+murilo2929@users.noreply.github.com>
2021-12-03 10:20:38 -0300

    discord logs By Fernando

---
+++ [SERP]/serp_discord_logs/webhookG.lua
@@ -0,0 +1,14 @@
+--[[
+	Author: Fernando
+
+	List of webhooks: name => URL
+]]
+
+webhook_list = {
+
+	-- Example:
+	-- 	name can be whatever you want, it is what you will use to trigger it
+	-- 	webhook URL must be the one you copied from channel settings -> Integrations -> Webhooks -> Your webhook -> Copy URL
+
+	["anuncio-logs"] = "https://discord.com/api/webhooks/916317595542650900/8Vg-7A6_Uj6BIZbBtRGgd093wViINyIB4pGEwcc2O6OfEQste_pJMysa83zqfhMoJNLs",
+}
