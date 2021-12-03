b929d962c9b6da329d8839b3d7cb840c32d44c84
murilo2929 <69065854+murilo2929@users.noreply.github.com>
2021-12-02 23:18:55 -0300

    sistema neon

---
+++ [SERP]/serp_neon/server.lua
@@ -0,0 +1,42 @@
+﻿addEvent("tuning->Neon", true)
+addEventHandler("tuning->Neon", root, function(vehicle, neon)
+	if vehicle then
+		triggerClientEvent(root, "tuning->Neon", root, vehicle, neon)
+	end
+end)
+local neonList = {
+	["0"] = "null",
+	["1"] = "white",
+	["2"] = "blue",
+	["3"] = "green",
+	["4"] = "red",
+	["5"] = "yellow",
+	["6"] = "pink",
+	["7"] = "orange",
+	["8"] = "lightblue",
+	["9"] = "rasta",
+	["10"] = "ice"
+}
+addCommandHandler("setneon",
+	function(player, cmd, vehID, color)
+		if exports.integration:isPlayerScripter(player) then
+			if not vehID or not color or not neonList[tostring(color)] then
+				outputChatBox("#ffffff /setneon [Veículo ID] [Cor (1-10)]", player, 0, 0, 255, true)
+				return
+			end
+			local vehicle = exports.ed_pool:getElement('vehicle', vehID)
+			if vehicle and isElement(vehicle) then
+				local color = neonList[tostring(color)]
+				if not color then return end
+				if color == "null" then color = false end
+				outputChatBox(color, player)
+				setElementData(vehicle, "tuning.neon", color)
+				local dbid = tonumber(getElementData(vehicle, "dbid")) or -1
+				dbExec(exports.mysql:getConn(), "UPDATE vehicles SET neon='"..tostring(color).."' WHERE id='"..(dbid).."'")
+				triggerClientEvent(root, "delNeon", root, vehicle, false)
+				triggerClientEvent(root, "addNeon", root, vehicle, color)
+				outputChatBox("#ffffff Operação bem-sucedida, neon do veículo alterada com sucesso.", player, 0, 255, 0, true)
+			end
+		end
+	end
+)
