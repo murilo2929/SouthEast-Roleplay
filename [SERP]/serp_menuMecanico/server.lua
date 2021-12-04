2180445fa5df0ad2b2c7b000a0491e1e405aa175
murilo2929 <69065854+murilo2929@users.noreply.github.com>
2021-12-04 18:21:18 -0300

    Update server.lua

--- [SERP]/serp_menuMecanico/server.lua
+++ [SERP]/serp_menuMecanico/server.lua
@@ -2,8 +2,8 @@ addCommandHandler("tunar",
 	function(player, cmd)
 		if player:getData("job") == 5 then
 			if player:getData("mechanic:gui:state") then 
-				exports["ed_infobox"]:addBox(player, "error", "Bu panel zaten şu anda ekranında")
-				outputChatBox( "Painel já está aberto", player, 255, 255, 255 )
+				exports["serp_infobox"]:addBox(player, "error", "Painel já está aberto")
+				--outputChatBox( "Painel já está aberto", player, 255, 255, 255 )
 			return end  
 			triggerClientEvent(player, "mechanic:gui", player, false, false, true)
 		end
